d4bbdfadda864d924beadfbff46bd65a5771d92b
nguyenvanphu45 <75570455+nguyenvanphu45@users.noreply.github.com>
2021-01-18 08:50:45 +0700

    bt2

---
+++ bt/bt3.java
@@ -0,0 +1,20 @@
+package bt;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class bt3 {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.print("Nhập số nguyên dương n = ");
+        int n = scanner.nextInt();
+
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 1; i < n + 1; i++) {
+            map.put(i, i * i);
+        }
+        System.out.println(map);
+    }
+}
---
+++ bt/bt6.java
@@ -0,0 +1,9 @@
+package bt;
+
+public class bt6 {
+    public static void main(String[] args) {
+        String str1 = "hoc java co ban den nang cao.";
+        String str2 = "java co ban";
+        System.out.println(str1.contains(str2));
+    }
+}
